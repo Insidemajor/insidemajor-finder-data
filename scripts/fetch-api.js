34eaba09abeaf9696b94afe6be30c64ab36e721f
Inside Major <contact@insidemajor.com>
2025-09-25 17:34:13 +0900

    Update fetch-api.js

--- scripts/fetch-api.js
+++ scripts/fetch-api.js
@@ -10,31 +10,30 @@ jobs:
     runs-on: ubuntu-latest
 
     steps:
-      - name: Checkout repo
-        uses: actions/checkout@v3
+    - name: Checkout repo
+      uses: actions/checkout@v3
 
-      - name: Set up Node.js
-        uses: actions/setup-node@v3
-        with:
-          node-version: '18'
+    - name: Set up Node.js
+      uses: actions/setup-node@v3
+      with:
+        node-version: '18'
 
-      - name: Install dependencies
-        run: npm install
-        working-directory: ./scripts
+    - name: Install dependencies
+      run: npm install
+      working-directory: ./scripts
 
-      - name: Run fetch-api script
-        env:
-          COLLEGE_SCORECARD_API_KEY: ${{ secrets.COLLEGE_SCORECARD_API_KEY }}
-        run: node fetch-api.js
-        working-directory: ./scripts
+    - name: Run fetch-api script
+      env:
+        COLLEGE_SCORECARD_API_KEY: ${{ secrets.COLLEGE_SCORECARD_API_KEY }}
+      run: node fetch-api.js
+      working-directory: ./scripts
 
-      - name: Commit and push updated data
-        run: |
-          git config --global user.name 'github-actions'
-          git config --global user.email 'actions@github.com'
-          git add ../data/filtered_data.json
-          git commit -m 'Update filtered data'
-          git push
-        working-directory: ./scripts
-        continue-on-error: true
-
+    - name: Commit and push updated data
+      run: |
+        git config --global user.name "github-actions"
+        git config --global user.email "actions@github.com"
+        git add ../data/filtered_data.json
+        git commit -m "Update filtered data"
+        git push
+      working-directory: ./scripts
+      continue-on-error: true
